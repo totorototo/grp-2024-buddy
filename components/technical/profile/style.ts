10b4a9b89510ef1915198660167486dd1479f107
toto rototo <el.totorototo@gmail.com>
2024-07-24 12:32:29 +0200

    wip: fix scrollbar issues for chrome ios

--- components/technical/profile/style.ts
+++ components/technical/profile/style.ts
@@ -19,11 +19,11 @@ const style = (Component: FunctionComponent<ProfileProps>) => styled(Component)`
   }
 
   .sections-container {
+    -webkit-overflow-scrolling: touch;
+    overflow-y: auto;
+    scrollbar-width: none; /* Firefox */
+    -ms-overflow-style: none; /* Internet Explorer 10+ */
     margin-top: env(safe-area-inset-top);
-    scrollbar-width: none;
-    ::-webkit-scrollbar {
-      display: none; /* Safari and Chrome */
-    }
     position: absolute;
     height: 70%;
     bottom: 0;
@@ -31,20 +31,33 @@ const style = (Component: FunctionComponent<ProfileProps>) => styled(Component)`
     width: 100vw;
     scroll-snap-type: x mandatory;
     display: flex;
-    overflow: auto;
+
     flex: none;
     flex-flow: row nowrap;
   }
 
-  .svg-container {
-    scrollbar-width: none;
-    ::-webkit-scrollbar {
-      display: none; /* Safari and Chrome */
+  /* WebKit and Blink (Chrome, Safari, newer versions of Opera) */
+  .sections-container::-webkit-scrollbar {
+    width: 0;
+    height: 0;
+    display: none;
+  }
+
+  @supports (-webkit-overflow-scrolling: touch) {
+    .sections-container {
+      overflow-y: scroll;
+      -webkit-overflow-scrolling: touch;
     }
+  }
+
+  .svg-container {
+    -webkit-overflow-scrolling: touch;
+    overflow-y: auto;
+    scrollbar-width: none; /* Firefox */
+    -ms-overflow-style: none; /* Internet Explorer 10+ */
     position: absolute;
     bottom: 0;
     left: 0;
-    overflow-y: scroll;
     font-size: 0.6rem;
 
     g:nth-child(even) {
@@ -65,6 +78,20 @@ const style = (Component: FunctionComponent<ProfileProps>) => styled(Component)`
         font-size: 0.6rem;
         color: var(--color-text);
       }
+    }
+  }
+
+  /* WebKit and Blink (Chrome, Safari, newer versions of Opera) */
+  .svg-container::-webkit-scrollbar {
+    width: 0;
+    height: 0;
+    display: none;
+  }
+
+  @supports (-webkit-overflow-scrolling: touch) {
+    .svg-container {
+      overflow-y: scroll;
+      -webkit-overflow-scrolling: touch;
     }
   }
 `;
